505f62ea4e16adade5abc03c832749c8ee284a2e
Amadeo <baghetcat@gmail.com>
2024-06-29 23:59:37 +0300

    develop

---
+++ delete.sh
@@ -0,0 +1,155 @@
+#!/bin/bash
+
+rm -rfv ~/Library/Caches/*
+
+rm -rfv ~/Library/Application\ Support/Slack/Cache/*
+
+rm -rfv ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
+
+rm -rfv ~/Library/Group\ Containers/6N38VWS5BX.ru.keepcoder.Telegram/account-570841890615083515/postbox/*
+
+rm -rfv ~/Library/Caches
+
+rm -rfv ~/Library/Application\ Support/Code/Code\ Cache
+
+rm -rfv ~/Library/Application\ Support/Slack/Cache
+
+rm -rfv ~/Library/Application\ Support/Slack/Code\ Cache
+
+rm -rfv ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage
+
+rm -rfv ~/Library/Application\ Support/Code/User/workspaceStorage
+
+rm -rf ~/Library/Caches/*
+
+rm -rf ~/Library/42_cache
+
+find ~/ -name ".DS_Store" -print -delete 2> /dev/null
+
+find ~/ -name "**.42_cache_bak**" -print -delete 2> /dev/null
+
+rm -rf ~/.zcompdump*
+
+rm -rf .Trash/*
+
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+
+rm -rf ~/Library/Caches/*
+
+rm -rf ~/Library/42_cache
+
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
+
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+
+rm -rf ~/Library/Caches/*
+
+rm -rf ~/Library/42_cache
+
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
+
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+
+rm -rf ~/Library/Developer/CoreSimulator/Caches/
+
+rm -rf ~/Library/Developer/CoreSimulator/Devices/
+
+rm -rf ~/Library/Logs/
+
+rm -rf ~/Library/Google/GoogleSoftwareUpdate/
+
+rm -rf ~/Library/Containers/com.apple.Safari/Data/Library/Caches/
+
+rm -rf ~/Library/Application\ Support/Code/CachedData/
+
+rm -rf ~/Library/Application\ Support/Slack/logs
+
+rm -rf ~/Library/Application\ Support/zoom.us/AutoUpdater
+
+rm -rf ~/Library/Application\ Support/Google/Chrome/BrowserMetrics/.
+
+rm -rf ~/Library/Application\ Support/Google/Chrome/BrowserMetrics-spare.pma
+
+rm -rf ~/Library/Application\ Support/Google/Chrome/GrShaderCache/GPUCache/
+
+rm -rf ~/Library/Application\ Support/Google/Chrome/Default/Local\ Extension Settings/cjpalhdlnbpafiamejdnhcphjbkeiagm/lost
+
+rm -rf ~/Library/Application\ Support/Code/Cache/
+
+rm -rf ~/Library/Application\ Support/Code/CachedExtensionVSIXs/
+
+rm -rf ~/Library/Application\ Support/Code/Code\ Cache/
+
+rm -rf ~/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/CacheStorage/
+
+rm -rf  ~/Library/Application\ Support/Google/Chrome/Default/Service\ Worker/ScriptCache/
+
+rm -rf ~/Library/Application\ Support/Google/Chrome/ShaderCache/GPUCache/
+
+rm -rf ~/Library/Application\ Support/Code/CachedExtensions/
+
+rm -rf ~/Library/Application\ Support/Code/logs/
+
+rm -rf ~/Library/Application\ Support/Code/Service\ Worker/CacheStorage/
+
+rm -rf ~/Library/Application\ Support/Code/Service\ Worker/ScriptCache/
+
+rm -rf ~/Library/Application\ Support/Code/User/workspaceStorage/
+
+rm -rf ~/.Trash/
+
+rm -rfv ~/Library/Caches/*
+
+rm -rfv ~/Library/Application\ Support/Slack/Cache/*
+
+rm -rfv ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
+
+rm -rfv ~/Library/Group\ Containers/6N38VWS5BX.ru.keepcoder.Telegram/account-570841890615083515/postbox/*
+
+rm -rfv ~/Library/Caches
+
+find ~/ -name ".DS_Store" -print -delete 2> /dev/null
+
+find ~/ -name "**.42_cache_bak**" -print -delete 2> /dev/null
+
+rm -rf ~/.zcompdump*
+
+rm -rf .Trash/*
+
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+
+rm -rf ~/Library/Caches/*
+
+rm -rf ~/Library/42_cache
+
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
+
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+
+rm -rf ~/Library/Caches/*
+
+rm -rf ~/Library/42_cache
+
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
+
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
+
+rm -rf ~/Library/Application\ Support/Slack/Cache/
+
+rm -rf ~/Library/Developer/CoreSimulator/Caches/
+
+rm -rf ~/Library/Developer/CoreSimulator/Devices/
+
+rm -rf ~/Library/Logs/
+
+rm -rf ~/Library/Google/GoogleSoftwareUpdate/
+
+rm -rf ~/Library/Containers/com.apple.Safari/Data/Library/Caches/
+
+rm -rf ~/Library/Application\ Support/Code/CachedData/Zigfreed
